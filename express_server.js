86713f87a69e4c5b0eaea425f3851c9a8dfbe3ae
jameshagans <jamesy.h95@gmail.com>
2023-03-22 00:54:01 +0000

    Modify _header.ejs to display a username using cookie-parser

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express(); // Create server connection - initialize app 
 const PORT = 8080; // default port 8080
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+
 
 //generate a random string code for short URLS
 function generateRandomString() {
@@ -44,14 +48,15 @@ app.get("/u/:id", (req, res) => {
 
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 
@@ -63,26 +68,15 @@ app.get("/urls/:id", (req, res) => {
   //http://localhost:3000   /urls/x26dh => req.params.id
 
   const IDs = req.params.id;
-  const templateVars = { id: IDs, longURL: urlDatabase[IDs] };
+  const templateVars = {
+    username: req.cookies["username"],
+    id: IDs, longURL: urlDatabase[IDs]
+  };
   console.log("test ", templateVars); // assigned req.params.id to a variable IDs 
   res.render("urls_show", templateVars);
 });
 
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -90,7 +84,6 @@ app.get("/urls.json", (req, res) => {
 
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
   const id = generateRandomString();
   urlDatabase[id] = req.body.longURL;
   console.log(`id is: ${id} and url is ${req.body.longURL}`);
@@ -107,11 +100,10 @@ app.post('/urls/:id/delete', (req, res) => {
 
 app.post('/login', (req, res) => {
   res.cookie('username', req.body.username);
-  console.log('cookie', req.body.username)
-  res.redirect('/urls'); 
+  console.log('Parsed cookie: ', req.cookies["username"]);
+  res.redirect('/urls');
 
-})
-
+});
 
 
 
@@ -121,7 +113,7 @@ app.post("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
   console.log('shortURL: ', shortURL);
   urlDatabase[shortURL] = req.body.Edit;
-res.redirect('/urls')
+  res.redirect('/urls');
 });
 
 
